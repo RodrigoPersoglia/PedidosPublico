c1ac99442c00fe48dbce1883c4448b1ec0e74070
Rodrigo <rodrigo.persoglia@gmail.com>
2021-12-02 11:19:43 -0300

    apertura unica de formulario 02-12

--- Login/Principal.cs
+++ Login/Principal.cs
@@ -22,9 +22,30 @@ namespace Login
 			InitializeComponent(); }
 
 		void AgregarToolStripMenuItemClick(object sender, EventArgs e)
-		{ AgregarArticulo nuevoart = new AgregarArticulo();
-			nuevoart.MdiParent = this;
-			nuevoart.Show();
+		{
+			int contador = 0;
+			foreach (Form f in Application.OpenForms)
+			{
+				if (f is AgregarArticulo)
+				{
+					f.Show();
+					if (f.WindowState == FormWindowState.Minimized)
+						f.WindowState = FormWindowState.Normal;
+					f.BringToFront();
+					contador++;
+					return;
+				}
+
+			}
+			if (contador == 0)
+			{
+				AgregarArticulo nuevoart = new AgregarArticulo();
+				nuevoart.MdiParent = this;
+				nuevoart.Show();
+			}
+			contador = 0;
+
+
 			
 	
 		}
@@ -32,16 +53,55 @@ namespace Login
 
 		void EliminarToolStripMenuItemClick(object sender, EventArgs e)
 		{
-			EliminarArticulo elimart = new EliminarArticulo();
-			elimart.MdiParent = this;
-			elimart.Show();
+			int contador = 0;
+			foreach (Form f in Application.OpenForms)
+			{
+				if (f is EliminarArticulo)
+				{
+					f.Show();
+					if (f.WindowState == FormWindowState.Minimized)
+						f.WindowState = FormWindowState.Normal;
+					f.BringToFront();
+					contador++;
+					return;
+				}
+
+			}
+			if (contador == 0)
+			{
+				EliminarArticulo elimart = new EliminarArticulo();
+				elimart.MdiParent = this;
+				elimart.Show();
+			}
+			contador = 0;
+
+
 	
 		}
 		void ModifArtClick(object sender, EventArgs e)
-		{ 
-			ModificarArticulo nuevoart = new ModificarArticulo();
-			nuevoart.MdiParent = this;
-			nuevoart.Show();
+		{
+			int contador = 0;
+			foreach (Form f in Application.OpenForms)
+			{
+				if (f is ModificarArticulo)
+				{
+					f.Show();
+					if (f.WindowState == FormWindowState.Minimized)
+						f.WindowState = FormWindowState.Normal;
+					f.BringToFront();
+					contador++;
+					return;
+				}
+
+			}
+			if (contador == 0)
+			{
+				ModificarArticulo nuevoart = new ModificarArticulo();
+				nuevoart.MdiParent = this;
+				nuevoart.Show();
+			}
+			contador = 0;
+
 			
 	
 		}
@@ -114,26 +174,79 @@ namespace Login
 		
 		
 		void VerListadoToolStripMenuItemClick(object sender, EventArgs e)
-		{ 
+		{
+			int contador = 0;
+			foreach (Form f in Application.OpenForms)
+			{
+				if (f is ListadoArticulos)
+				{
+					f.Show();
+					if (f.WindowState == FormWindowState.Minimized)
+						f.WindowState = FormWindowState.Normal;
+					f.BringToFront();
+					contador++;
+					return;
+				}
+
+			}
+			if (contador == 0)
+			{
 			ListadoArticulos listado = new ListadoArticulos("");
 			listado.MdiParent = this;
-			listado.Show();			
+			listado.Show();		
+			}
+			contador = 0;
 			
 		}
 		
 		void AgregarToolStripMenuItem1Click(object sender, EventArgs e)
-		{	AgregarCliente clientenuevo = new AgregarCliente();
-			clientenuevo.MdiParent = this;
-			clientenuevo.Show();
-			
+		{
+			int contador = 0;
+			foreach (Form f in Application.OpenForms)
+			{
+				if (f is AgregarCliente)
+				{
+					f.Show();
+					if (f.WindowState == FormWindowState.Minimized)
+						f.WindowState = FormWindowState.Normal;
+					f.BringToFront();
+					contador++;
+					return;
+				}
+
+			}
+			if (contador == 0)
+			{
+				AgregarCliente clientenuevo = new AgregarCliente();
+				clientenuevo.MdiParent = this;
+				clientenuevo.Show();
+			}
+			contador = 0;
 		}
 		
 		void ModificarToolStripMenuItemClick(object sender, EventArgs e)
 		{
-			ModificarCliente modificarcli = new ModificarCliente();
-			modificarcli.MdiParent = this;
-			modificarcli.Show();
-			
+			int contador = 0;
+			foreach (Form f in Application.OpenForms)
+			{
+				if (f is ModificarCliente)
+				{
+					f.Show();
+					if (f.WindowState == FormWindowState.Minimized)
+						f.WindowState = FormWindowState.Normal;
+					f.BringToFront();
+					contador++;
+					return;
+				}
+
+			}
+			if (contador == 0)
+			{
+				ModificarCliente modificarcli = new ModificarCliente();
+				modificarcli.MdiParent = this;
+				modificarcli.Show();
+			}
+			contador = 0;
 		}
 
         private void pruebaToolStripMenuItem_Click(object sender, EventArgs e)
@@ -143,47 +256,152 @@ namespace Login
 
         private void PedidoToolStripMenuItem_Click(object sender, EventArgs e)
         {
-			AgregarPedido agregarpe = new AgregarPedido();
-			agregarpe.MdiParent = this;
-			agregarpe.Show();
+			int contador = 0;
+			foreach (Form f in Application.OpenForms)
+			{
+				if (f is AgregarPedido)
+				{
+					f.Show();
+					if (f.WindowState == FormWindowState.Minimized)
+						f.WindowState = FormWindowState.Normal;
+					f.BringToFront();
+					contador++;
+					return;
+				}
 
+			}
+			if (contador == 0)
+			{
+				AgregarPedido agregarpe = new AgregarPedido();
+				agregarpe.MdiParent = this;
+				agregarpe.Show();
+			}
+			contador = 0;
 		}
 
         private void listadoAcredoresToolStripMenuItem_Click(object sender, EventArgs e)
         {
-			ListadoPedidos listPed = new ListadoPedidos(usuario.User);
-			listPed.MdiParent = this;
-			listPed.Show();
+			int contador = 0;
+			foreach (Form f in Application.OpenForms)
+			{
+				if (f is ListadoPedidos)
+				{
+					f.Show();
+					if (f.WindowState == FormWindowState.Minimized)
+						f.WindowState = FormWindowState.Normal;
+					f.BringToFront();
+					contador++;
+					return;
+				}
+
+			}
+			if (contador == 0)
+			{
+				ListadoPedidos listPed = new ListadoPedidos(usuario.User);
+				listPed.MdiParent = this;
+				listPed.Show();
+			}
+			contador = 0;
 		}
 
         private void listadoDeudoresToolStripMenuItem_Click(object sender, EventArgs e)
         {
-			ModificarPedido modificarpedido = new ModificarPedido();
-			modificarpedido.MdiParent = this;
-			modificarpedido.Show();
+			int contador = 0;
+			foreach (Form f in Application.OpenForms)
+			{
+				if (f is ModificarPedido)
+				{
+					f.Show();
+					if (f.WindowState == FormWindowState.Minimized)
+						f.WindowState = FormWindowState.Normal;
+					f.BringToFront();
+					contador++;
+					return;
+				}
+
+			}
+			if (contador == 0)
+			{
+				ModificarPedido modificarpedido = new ModificarPedido();
+				modificarpedido.MdiParent = this;
+				modificarpedido.Show();
+			}
+			contador = 0;
 		}
 
         private void agregarToolStripMenuItem2_Click(object sender, EventArgs e)
         {
-			AgregarMatriz agregarMat = new AgregarMatriz();
-			agregarMat.MdiParent = this;
-			agregarMat.Show();
+			int contador = 0;
+			foreach (Form f in Application.OpenForms)
+			{
+				if (f is AgregarMatriz)
+				{
+					f.Show();
+					if (f.WindowState == FormWindowState.Minimized)
+						f.WindowState = FormWindowState.Normal;
+					f.BringToFront();
+					contador++;
+					return;
+				}
+
+			}
+			if (contador == 0)
+			{
+				AgregarMatriz agregarMat = new AgregarMatriz();
+				agregarMat.MdiParent = this;
+				agregarMat.Show();
+			}
+			contador = 0;
 		}
 
         private void ciudadesToolStripMenuItem_Click(object sender, EventArgs e)
         {
-			AgregarCiudad ciudad = new AgregarCiudad();
-			ciudad.MdiParent = this;
-			ciudad.Show();
+			int contador = 0;
+			foreach (Form f in Application.OpenForms)
+			{
+				if (f is AgregarCiudad)
+				{
+					f.Show();
+					if (f.WindowState == FormWindowState.Minimized)
+						f.WindowState = FormWindowState.Normal;
+					f.BringToFront();
+					contador++;
+					return;
+				}
 
-
+			}
+			if (contador == 0)
+			{
+				AgregarCiudad ciudad = new AgregarCiudad();
+				ciudad.MdiParent = this;
+				ciudad.Show();
+			}
+			contador = 0;
 		}
 
         private void detallaDeFabricaciónToolStripMenuItem_Click(object sender, EventArgs e)
         {
-			DetalleFabricacion dt = new DetalleFabricacion();
-			dt.MdiParent = this;
-			dt.Show();
+			int contador = 0;
+			foreach (Form f in Application.OpenForms)
+			{
+				if (f is DetalleFabricacion)
+				{
+					f.Show();
+					if (f.WindowState == FormWindowState.Minimized)
+						f.WindowState = FormWindowState.Normal;
+					f.BringToFront();
+					contador++;
+					return;
+				}
+
+			}
+			if (contador == 0)
+			{
+				DetalleFabricacion dt = new DetalleFabricacion();
+				dt.MdiParent = this;
+				dt.Show();
+			}
+			contador = 0;
 		}
 
         private void button1_Click(object sender, EventArgs e)
@@ -215,33 +433,85 @@ namespace Login
 
         private void verListadoToolStripMenuItem1_Click(object sender, EventArgs e)
         {
-			ListadoClientes listadoCli = new ListadoClientes("");
-			listadoCli.MdiParent = this;
-			listadoCli.Show();
+			int contador = 0;
+			foreach (Form f in Application.OpenForms)
+			{
+				if (f is ListadoClientes)
+				{
+					f.Show();
+					if (f.WindowState == FormWindowState.Minimized)
+						f.WindowState = FormWindowState.Normal;
+					f.BringToFront();
+					contador++;
+					return;
+				}
+
+			}
+			if (contador == 0)
+			{
+				ListadoClientes listadoCli = new ListadoClientes("");
+				listadoCli.MdiParent = this;
+				listadoCli.Show();
+			}
+			contador = 0;
+
 
 		}
 
         private void modificarToolStripMenuItem1_Click(object sender, EventArgs e)
         {
-			ModificarMatriz modmat = new ModificarMatriz();
-			modmat.MdiParent = this;
-			modmat.Show();
+			int contador = 0;
+			foreach (Form f in Application.OpenForms)
+			{
+				if (f is ModificarMatriz)
+				{
+					f.Show();
+					if (f.WindowState == FormWindowState.Minimized)
+						f.WindowState = FormWindowState.Normal;
+					f.BringToFront();
+					contador++;
+					return;
+				}
+
+			}
+			if (contador == 0)
+			{
+				ModificarMatriz modmat = new ModificarMatriz();
+				modmat.MdiParent = this;
+				modmat.Show();
+			}
+			contador = 0;
 		}
 
         private void ayudaToolStripMenuItem_Click(object sender, EventArgs e)
         {
-			Contacto cont = new Contacto();
-			cont.MdiParent = this;
-			cont.Show();
+			int contador = 0;
+			foreach (Form f in Application.OpenForms)
+			{
+				if (f is Contacto)
+				{
+					f.Show();
+					if (f.WindowState == FormWindowState.Minimized)
+						f.WindowState = FormWindowState.Normal;
+					f.BringToFront();
+					contador++;
+					return;
+				}
+
+			}
+			if (contador == 0)
+			{
+				Contacto cont = new Contacto();
+				cont.MdiParent = this;
+				cont.Show();
+			}
+			contador = 0;
         }
 
-        private void proyecciónToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-			Proyeccion proye = new Proyeccion();
-			proye.MdiParent = this;
-			proye.Show();
+  //      private void proyecciónToolStripMenuItem_Click(object sender, EventArgs e)
+  //      {
 
-		}
+		//}
 
         private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
         {
@@ -253,58 +523,144 @@ namespace Login
 
         }
 
-        private void enProduccionToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-			EnProduccion produ = new EnProduccion();
-			produ.MdiParent = this;
-			produ.Show();
+        //private void enProduccionToolStripMenuItem_Click(object sender, EventArgs e)
+        //{
 
-        }
+        //}
 
         private void usuarioToolStripMenuItem_Click(object sender, EventArgs e)
         {
-			User gestionUsuario = new User();
-			gestionUsuario.MdiParent = this;
-			gestionUsuario.Show();
+			int contador = 0;
+			foreach (Form f in Application.OpenForms)
+			{
+				if (f is User)
+				{
+					f.Show();
+					if (f.WindowState == FormWindowState.Minimized)
+						f.WindowState = FormWindowState.Normal;
+					f.BringToFront();
+					contador++;
+					return;
+				}
+
+			}
+			if (contador == 0)
+			{
+				User gestionUsuario = new User();
+				gestionUsuario.MdiParent = this;
+				gestionUsuario.Show();
+			}
+			contador = 0;
 		}
 
-        private void reporteProducciónToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-			ReporteProduccion reporte = new ReporteProduccion();
-			reporte.MdiParent = this;
-			reporte.Show();
-		}
+        //private void reporteProducciónToolStripMenuItem_Click(object sender, EventArgs e)
+        //{
+        //    ReporteProduccion reporte = new ReporteProduccion();
+        //    reporte.MdiParent = this;
+        //    reporte.Show();
+        //}
 
 
 
         private void proyeccionToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-			Proyeccion proye = new Proyeccion();
-			proye.MdiParent = this;
-			proye.Show();
+        {//ok
+			int contador = 0;
+			foreach (Form f in Application.OpenForms)
+			{
+				if (f is Proyeccion)
+				{
+					f.Show();
+					if (f.WindowState == FormWindowState.Minimized)
+						f.WindowState = FormWindowState.Normal;
+					f.BringToFront();
+					contador++;
+					return;
+				}
+
+			}
+			if (contador == 0)
+			{
+				Proyeccion proye = new Proyeccion();
+				proye.MdiParent = this;
+				proye.Show();
+			}
+			contador = 0;
 
 		}
 
         private void enProducciónToolStripMenuItem_Click_1(object sender, EventArgs e)
-        {
-			EnProduccion produ = new EnProduccion();
-			produ.MdiParent = this;
-			produ.Show();
+        {//ok
+			int contador = 0;
+			foreach (Form f in Application.OpenForms)
+			{
+				if (f is EnProduccion)
+				{
+					f.Show();
+					if (f.WindowState == FormWindowState.Minimized)
+						f.WindowState = FormWindowState.Normal;
+					f.BringToFront();
+					contador++;
+					return;
+				}
+
+			}
+			if (contador == 0)
+			{
+				EnProduccion produ = new EnProduccion();
+				produ.MdiParent = this;
+				produ.Show();
+			}
+			contador = 0;
 		}
 
         private void reporteDeProducciónToolStripMenuItem_Click_1(object sender, EventArgs e)
         {
-			ReporteProduccion reporte = new ReporteProduccion();
-			reporte.MdiParent = this;
-			reporte.Show();
+			int contador = 0;
+			foreach (Form f in Application.OpenForms)
+			{
+				if (f is ReporteProduccion)
+				{
+					f.Show();
+					if (f.WindowState == FormWindowState.Minimized)
+						f.WindowState = FormWindowState.Normal;
+					f.BringToFront();
+					contador++;
+					return;
+				}
 
+			}
+			if (contador == 0)
+            {
+				ReporteProduccion reporte = new ReporteProduccion();
+				reporte.MdiParent = this;
+				reporte.Show();
+			}
+			contador = 0;
 		}
 
         private void toolStripMenuItem1_Click(object sender, EventArgs e)
         {
-			Nitrurado nitrurado = new Nitrurado();
-			nitrurado.MdiParent = this;
-			nitrurado.Show();
+			int contador = 0;
+			foreach (Form f in Application.OpenForms)
+			{
+				if (f is Nitrurado)
+				{
+					f.Show();
+					if (f.WindowState == FormWindowState.Minimized)
+						f.WindowState = FormWindowState.Normal;
+					f.BringToFront();
+					contador++;
+					return;
+				}
+
+			}
+			if (contador == 0)
+			{
+				Nitrurado nitrurado = new Nitrurado();
+				nitrurado.MdiParent = this;
+				nitrurado.Show();
+			}
+			contador = 0;
         }
     }
 }
